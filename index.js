342cea28983597939c3dc7153d4ae205add40f1d
Vinos Sarhad <vinos.sarhadd@gmail.com>
2024-02-13 13:02:20 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+ let currentUser="string";
+ let welcomeMessage="Welcome to Flatbook, "+currentUser +"!";
+
+
+let excitedWelcomeMessage="WELCOME TO FLATBOOK, "+ currentUser.toUpperCase(currentUser)  +"!";
+
+let firstLetter = currentUser.split('').splice(0, 1)[0];
+
+let shortGreeting= "Welcome, "+ firstLetter +"!"
+console.log(shortGreeting)
